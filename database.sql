e78c08af528085f7685d8f0f4d2231db2f2386d6
Conor Callahan <conorcallahan97@outlook.com>
2024-06-17 21:27:52 -0500

    Added the photos files into the projects and added the required code to the sql table chunk in the database.sql file.

--- database.sql
+++ database.sql
@@ -10,5 +10,8 @@ INSERT INTO "gallery"
 ("url", "title", "description")
 VALUES
 ('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.'),
-('images/goat_stache.png', 'Goat Stache!', 'Photo of a mustachioed goat taken at Glacier National Park.');
-  
+('images/goat_stache.png', 'Goat Stache!', 'Photo of a mustachioed goat taken at Glacier National Park.')
+('images/dog.jpeg', 'Sleeping Dog', 'Photo of my girlfriends family dog sleeping.'),
+('images/hot_dish.jpeg', 'Hottest Casserole', 'Multiple trays of my signature tater tot hot dish.'),
+('images/risky_brother.jpeg', 'Reckless Brother', 'My older brother hunting in the rockies and finding a helpful sign.')
+('images/kitties.jpeg', 'Cute Cats', 'My own personal pets, Baxter(orange) and Ozzy(dark/long hair)');
